3a483b62c107380affc1724f2ccb8751526c1054
nicola <casiraghinicola@gmail.com>
2019-07-13 19:17:45 +0200

    clean _pre and _post data

--- infercnv_filter_barcodes.R
+++ infercnv_filter_barcodes.R
@@ -2,12 +2,33 @@ library( Seurat )
 
 patients <- list.files("/icgc/dkfzlsdf/analysis/hipo2/hipo_K43R/MM_scRNAseq_data/K43R/Patients",full.names = T)
 
+clean_barcodes <- list()
+
 for(p in unique(patients)){
   message(basename(p))
   
+  tumor_files <- list.files(p,pattern = paste0("Tumor_"),full.names = T)
   
-  tumor_file <- list.files(p,pattern = paste0("Tumor_",basename(p),".rds"),full.names = T)
-  tumor <- readRDS(tumor_file)
+  get_barcodes <- function(tumor_files, pattern){
+    rds <- grep(tumor_files,pattern = pattern,value = T)
+    if(length(rds)>0){
+      tumor <- readRDS(rds)
+      tumorDF <- tumor@meta.data[grep(rownames(tumor@meta.data),pattern = "_T[[:digit:]]_"),]
+      return(rownames(tumorDF))
+    }
+  }
   
-  cat(unique(tumor@meta.data),'\n')
+  pre <- get_barcodes(tumor_files = tumor_files,pattern = "_pre.rds")
+  post <- get_barcodes(tumor_files = tumor_files,pattern = "_post.rds")
+
+  get_clean_barcode <- function(bc){
+    sample <- unlist(strsplit(bc,split = '_'))[2]
+    barcode <- unlist(strsplit(bc,split = '_'))[3]
+    return(paste0(barcode,'-1pos_',sample))
+  }
+  
+  clean_barcodes[[basename(p)]] <- unique(unlist(lapply(unique(c(pre,post)),FUN = get_clean_barcode)))
 }
+
+
+
