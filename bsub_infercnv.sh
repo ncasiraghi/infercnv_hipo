ae6261bcfc38af69797922ec8535b39fb7b8f05f
nicola <casiraghinicola@gmail.com>
2019-07-14 14:12:54 +0200

    minor changes

--- bsub_infercnv.sh
+++ bsub_infercnv.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 #BSUB -J infercnv
-#BSUB -q night 
-#BSUB -e infercnv.bsub.log 
-#BSUB -o infercnv.bsub.txt 
+#BSUB -q verylong 
+#BSUB -e bsub_infercnv.log 
+#BSUB -o bsub_infercnv.txt 
 
 module load R/3.5.1
 
-Rscript infercnv_analysis.R
+Rscript /icgc/dkfzlsdf/analysis/hipo2/hipo_K43R/InferCNV/infercnv_hipo/infercnv_analysis.R /icgc/dkfzlsdf/analysis/hipo2/hipo_K43R/InferCNV/infercnv_outs/clean_barcodes/ZPMZFJ/config_infercnv.R
