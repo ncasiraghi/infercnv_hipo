6219703ab08bc37077d05cb099a86d81cdd9f8f2
nicola <casiraghinicola@gmail.com>
2019-07-23 16:57:36 +0200

    get correct barcode name

--- postprocess_infercnv_outs.R
+++ postprocess_infercnv_outs.R
@@ -1,23 +1,32 @@
 library( phylogram )
 library( dendextend ) 
 
-infercnv_folder_outout <- "/icgc/dkfzlsdf/analysis/hipo2/hipo_K43R/InferCNV/infercnv_outs/clean_barcodes/TS6ZX9/results"
+infercnv.observations <- "/icgc/dkfzlsdf/analysis/hipo2/hipo_K43R/InferCNV/infercnv_outs/clean_barcodes/TS6ZX9/results/infercnv.preliminary.observations_dendrogram.txt"
 
 # get and visualize the dendrogram
 
-dend <- as.hclust( read.dendrogram(file.path(infercnv_folder_outout,"infercnv.observations_dendrogram.txt") ))
-class(dend)
-
+dend <- as.hclust(read.dendrogram(infercnv.observations))
 plot(dend,labels = FALSE,hang = -1,xlab = "scRNA",ylab = "distance")
 
 # cut the dendrogram 
-
 NCLONES <- 4
 
 tp <- color_branches(dend, k = NCLONES)
 plot(tp,leaflab = 'none')
 
 g <- cutree(dend,k = NCLONES)
-groups <- data.frame(barcode=gsub(names(g),pattern = "pos_N1",replacement = ""),
-                     cluster=as.numeric(g),
+
+# adjust barcodes
+clean_barcode <- function(b){
+  id <- unlist(strsplit(b,split = '_'))[2]
+  new <- paste0('TS6ZX9_',id,'_',gsub(b,pattern = "-1pos_T1|-1pos_N1",replacement = ""))
+  return(new)
+}
+
+barcodes <- unlist(lapply(names(g),clean_barcode))
+
+groups <- data.frame(BARCODE=barcodes,
+                     CLONE=as.numeric(g),
                      stringsAsFactors = F)
+
+save(groups,file = 'TS6ZX9_Barcodes_Clones.RData',compress = TRUE)
